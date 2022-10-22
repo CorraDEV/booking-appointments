395754099a780afa62fc4a87d6c62e75e7d2c3b8
corra <corrado.marzagalli@gmail.com>
2022-10-22 20:37:20 +0200

    file login corretto

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ require_once('config.php');
 $email = $connessione->real_escape_string($_POST['email']); 
 $password = $connessione->real_escape_string($_POST['password']);
 
-$sql = "select * from users where users.username='$username' and users.password='$password'";
+$sql = "select * from utenti where utenti.email='$email' and utenti.password='$password'";
 $riga = mysqli_query($connessione, $sql);
 
 if (mysqli_num_rows($riga)>0)

0c737ff30af72335b8984b7052ad498ff4320336
corra <corrado.marzagalli@gmail.com>
2022-10-22 20:24:29 +0200

    aggiunto e finito file login senza controlli

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<!-- script registrazione -->
+<!-- script login -->
 <?php
 
 require_once('config.php'); 
@@ -6,9 +6,11 @@ require_once('config.php');
 $email = $connessione->real_escape_string($_POST['email']); 
 $password = $connessione->real_escape_string($_POST['password']);
 
-$sql = "INSERT INTO utenti (EMAIL, PASSWORD) VALUES ('$email', '$password')";
+$sql = "select * from users where users.username='$username' and users.password='$password'";
+$riga = mysqli_query($connessione, $sql);
 
-if($connessione->query($sql) === true){    
+if (mysqli_num_rows($riga)>0)
+{
     header("location: index.php");
 }
 ?>
